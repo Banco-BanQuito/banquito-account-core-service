b125ba6c6471d1c3144e5af50397274e86d21326
Oscarf05 <ofchanataxi@espe.edu.ec>
2026-06-01 10:02:07 -0500

    Estructura inicial

---
+++ account-core/src/main/java/ec/edu/espe/banquito/accountcore/AccountCoreApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.banquito.accountcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountCoreApplication.class, args);
+    }
+
+}
